7c9bf98bc5f3ada568821c6b0e3224f4b5f676e2
Bilue <bilue@Bilues-MacBook-Pro.local>
2017-02-06 14:29:45 +1100

    Add startup script

---
+++ startup.sh
@@ -0,0 +1,12 @@
+etup password
+mkdir /root/.vnc/
+echo $VNC_PASSWD | vncpasswd -f > /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd
+
+# Start vnc server
+vncserver :1 -geometry 1280x800 -depth 24
+
+# Run TRiBot
+DISPLAY=:1 XAUTHORITY=/root/.Xauthority java -jar TRiBot_Loader.jar 2>&1 >> tribot_logs.log
+
+tail -F /root/.vnc/*.log
